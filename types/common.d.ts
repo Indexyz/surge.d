683f12381cbe90c70d590972b97460a8fe9391a5
Indexyz <jiduye@gmail.com>
2019-11-01 09:30:47 +0800

    feat: move class declare to type declare

--- types/common.d.ts
+++ types/common.d.ts
@@ -1,14 +1,17 @@
 /**
  * Surge utils class
  **/
-declare class $utils {
+type Util = {
     /**
      * Perform a GeoIP lookup. Results are in ISO 3166 code.
-     * 
+     *
      * @param ip the ip that you want to query
      */
-    static geoip(ip: string): string
+    geoip(ip: string): string
 }
+
+declare const $util: Util
+
 
 /**
  * Outbound Mode
@@ -22,67 +25,73 @@ declare enum OutboundMode {
 /**
  * Surge functional class
  */
-declare class $surge {
+type Surge = {
     /**
      * Set the outbound mode, the value should be 'direct', 'global-proxy', or 'rule'.Return true if successes
-     * 
+     *
      * @param mode outbound mode
      */
-    static setOutboundMode(mode: OutboundMode): boolean
+    setOutboundMode(mode: OutboundMode): boolean
 
     /**
      * Set http capture is enabled
-     * 
+     *
      * @param enabled Is enable http capture
      */
-    static setHTTPCaptureEnabled(enabled: boolean)
+    setHTTPCaptureEnabled(enabled: boolean)
 
     /**
      * Set Cellular Mode status
-     * 
+     *
      * @param enabled Is enable CellularMode
      */
-    static setCellularModeEnabled(enabled: boolean)
+    setCellularModeEnabled(enabled: boolean)
 
     /**
      * Set Rewrite status
      * @param enabled Is enable Rewrite
      */
-    static setRewriteEnabled(enabled: boolean)
+    setRewriteEnabled(enabled: boolean)
 
     /**
      * Set Enhanced Mode status, Surge Mac Only
      * @param enabled Is enable Enhanced Mode
      */
-    static setEnhancedModeEnabled(enabled: boolean)
+    setEnhancedModeEnabled(enabled: boolean)
 }
+
+declare const $surge: Surge
 
 /**
  * Script in surge
  */
-declare class $script {
+type Script = {
     /**
      * The script name which is being evaluating
      */
-    static name: string
+    name: string
     /**
      * The time when the current script starts.
      */
-    static startTime: Date
+    startTime: Date
 }
+
+declare const $script: Script
 
 /**
  * Surge notification
  */
-declare class $notification {
+type Notification = {
     /**
      * Post a notification.
-     * 
+     *
      * @param title Notification title
      * @param subtitle Notification subtitle
      * @param body Notification body
      */
-    static post(title: string, subtitle: string, body: string)
+    post(title: string, subtitle: string, body: string)
 }
+
+declare const $notification: Notification
 
 declare const $request: IncomingRequest | OutcomingRequest
